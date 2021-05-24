96483dfa4fe2de72c018f8063803abe273b28bc2
Peter A <peterayala@college.harvard.edu>
2021-05-23 21:06:27 -0400

    Revert "Updates"
    This reverts commit 99b3fbacde6c2644bdd807568f398a85900a7024.

---
+++ app/views/Index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Main from '../layouts/Main';
+
+const Index = () => (
+  <Main>
+    <article className="post" id="index">
+      <header>
+        <div className="title">
+          <h2><Link to="/">About this site</Link></h2>
+          <p>A personal website</p>
+        </div>
+      </header>
+      <p> Welcome to my website! Please feel free to read more <Link to="/about">about me</Link>,
+        check out my {' '}
+        <Link to="/resume">resume</Link>, or view  {' '}
+        <Link to="/projects">projects</Link>, {' '} I've worked on.
+      </p>
+    </article>
+  </Main>
+);
+
+export default Index;
